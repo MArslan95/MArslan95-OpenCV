b4b358df849973ad06658037a6d6ed39b37d3e9d
Muhammad Arslan <marslan93001@gmail.com>
2021-12-19 21:11:16 +0500

    Smoothing

--- Colorfiltering.py
+++ Colorfiltering.py
@@ -11,9 +11,13 @@ while True:
 
     mask = cv2.inRange(hsv, lower_red, upper_red)
     res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    kernel = np.ones((15, 15), np.float32)/225
+    smoothed = cv2.filter2D(res, -1, kernel)
     cv2.imshow('Frame', frame)
-    cv2.imshow('Mask', mask)
+    #cv2.imshow('Mask', mask)
     cv2.imshow('Res', res)
+    cv2.imshow('Smoothed', smoothed)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
